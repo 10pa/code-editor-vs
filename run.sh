2b5eb70c115effc749c7ae041e3eb7aeff8fb094
10pa <122980313+10pa@users.noreply.github.com>
2023-02-04 01:51:52 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 echo Installing a Code editor ...
 echo is take some time
-sudo apt update
-sudo apt install curl
+apt update
+apt install curl
+apt install sudo
 sudo curl -fsSL https://code-server.dev/install.sh | sh
 
 
